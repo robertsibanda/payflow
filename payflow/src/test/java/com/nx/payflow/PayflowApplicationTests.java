8f6f591cac194fcb52b1a7d2ca39a82f30380aa9
Robert Sibanda <robert.sibanda@1-grid.com>
2026-06-06 20:15:44 +0200

    project init

---
+++ payflow/src/test/java/com/nx/payflow/PayflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nx.payflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
